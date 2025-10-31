5da5be37e43864d9b82e1b14860817995d02df6a
Firas Chabchoub <firaschabchoub@hotmail.com>
2025-11-01 00:02:15 +0100

    Implement standalone GraphQL server with Jetty and update dependencies

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,29 @@
-import repository.ModuleBusiness;
-import repository.UniteEnseignementBusiness;
+import graphql.GraphQLEndpoint;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class main {
-    public static void main(String[] args) {
-        ModuleBusiness mb = new ModuleBusiness();
-        UniteEnseignementBusiness ueb = new UniteEnseignementBusiness();
+    public static void main(String[] args) throws Exception {
+        int port = 5003;
+        Server server = new Server(port);
+
+        // Create context at a root path
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        // Register GraphQL endpoint servlet at /graphql
+        ServletHolder graphqlServlet = new ServletHolder(new GraphQLEndpoint());
+        context.addServlet(graphqlServlet, "/graphql");
+
+        try {
+            server.start();
+            System.out.println("GraphQL server started on http://localhost:" + port + "/graphql");
+            server.join();
+        } finally {
+            server.stop();
+            server.destroy();
+        }
     }
 }
